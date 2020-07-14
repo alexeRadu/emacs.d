b681b6cd9f16a01b5b0378211f0e9faecf272bce
Radu Alexe <alexeRadu2007@gmail.com>
2020-07-14 23:24:16 +0300

    remove relative line numbers

--- init.el
+++ init.el
@@ -140,19 +140,6 @@
   )
 
 ;; -----------------------------------------------------------------------------
-;; Line numbers
-;;
-;; Enable linum-mode for version of emacs >= 26.0.50.
-;; TODO: for version < 25.0.50 should either look for a solution or throw a (warning)
-;; message.
-(when (version<= "26.0.50" emacs-version)
-  (setq display-line-numbers-type 'relative)
-  (global-display-line-numbers-mode))
-
-;; TODO: various customizations that exist @ https://www.emacswiki.org/emacs/LineNumbers.
-;; TODO: selectively enable line-numbers depending on the file type.
-
-;; -----------------------------------------------------------------------------
 ;; Ruler at 80 chars
 ;;
 ;; I use fill-column-indicator.
