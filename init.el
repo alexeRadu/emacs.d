47ae2caa65d6354556cb24847fe6dfdc3b093a67
Radu Alexe <alexeRadu2007@gmail.com>
2020-07-14 23:34:41 +0300

    display line numbers

--- init.el
+++ init.el
@@ -139,6 +139,22 @@
   :bind (("C-x g" . magit-status))
   )
 
+(require 'display-line-numbers)
+(defcustom display-line-numbers-exempt-modes '(vterm-mode eshell-mode shell-mode term-mode ansi-term-mode)
+  "Major modes on which to disable the linum mode, exempts them from global requirement"
+  :group 'display-line-numbers
+  :type 'list
+  :version "green")
+
+(defun display-line-numbers--turn-on ()
+  "turn on line numbers but excempting certain majore modes defined in `display-line-numbers-exempt-modes'"
+  (if (and
+       (not (member major-mode display-line-numbers-exempt-modes))
+       (not (minibufferp)))
+      (display-line-numbers-mode)))
+
+(global-display-line-numbers-mode)
+
 ;; -----------------------------------------------------------------------------
 ;; Ruler at 80 chars
 ;;
