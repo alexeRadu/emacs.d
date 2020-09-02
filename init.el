9a3b8d0d68da2feeac67f63e313fa8f252401341
Radu Alexe <alexeRadu2007@gmail.com>
2020-09-02 20:56:40 +0300

    adding evil bindings
    No we want to customize the emacs experience.

--- init.el
+++ init.el
@@ -177,3 +177,11 @@
 (define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
 (global-fci-mode 1)
 (setq fci-rule-column 80)
+
+;; -----------------------------------------------------------------------------
+;; Evil Mode
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+(require 'evil)
+(evil-mode)
