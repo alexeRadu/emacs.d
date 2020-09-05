810db6113adcd91a30a0493a7ed7da78b7dde803
Radu Alexe <alexeRadu2007@gmail.com>
2020-09-05 09:31:24 +0300

    use doom-one theme

--- init.el
+++ init.el
@@ -8,9 +8,12 @@
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(custom-enabled-themes nil)
+ '(custom-safe-themes
+   (quote
+    ("e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "9ffe970317cdfd1a9038ee23f4f5fe0b28b99950281799e4397e1a1380123147" default)))
  '(package-selected-packages
    (quote
-    (zenburn-theme evil-magit fill-column-indicator evil magit svg company counsel-gtags ggtags neotree avy counsel swiper projectile org-bullets use-package))))
+    (doom-themes zenburn-theme evil-magit fill-column-indicator evil magit svg company counsel-gtags ggtags neotree avy counsel swiper projectile org-bullets use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -209,3 +212,7 @@
 (require 'doom-modeline)
 (doom-modeline-mode 1)
 (setq doom-modeline-modal-icon nil)
+
+;; -----------------------------------------------------------------------------
+;; Doom themes
+(load-theme 'doom-one t)
