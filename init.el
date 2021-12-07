d6b71c139da621e48ac824eae3488a359ef5088e
Radu Alexe <radu.alexe@nxp.com>
2021-12-07 16:25:40 +0200

    add tab settings for the python-mode

--- init.el
+++ init.el
@@ -308,3 +308,12 @@
 	  (setq count (1+ count))
 	  (kill-buffer buffer))
 	(message "Killed %i dired buffer(s)." count)))))
+
+;; -----------------------------------------------------------------------------
+;; Python settings
+
+(add-hook `python-mode-hook
+	  (lambda ()
+	    (setq indent-tabs-mode t)
+	    (setq python-indent 4)
+	    (setq tab-width 4)))
